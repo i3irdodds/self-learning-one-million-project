df3a7365fd77d31cc3241ba704fcd1521ad371b7
Surasak <moomdate@surasaks-MacBook-Pro.local>
2020-10-10 12:44:08 +0700

    Add Craco for custom theme.

---
+++ src/components/Main-components/Main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'; // let's also import Component
+import { Layout, Button } from 'antd';
+import './Main.less';
+
+export default class Main extends React.Component {
+  state = {
+    size: 'large',
+  };
+
+  render() {
+    // const { size } = this.state;
+    const { Header, Footer, Sider, Content } = Layout;
+
+    return (
+      <React.Fragment>
+        <Layout>
+          <Sider>
+            <h1 style={{ color: 'red' }}> this header side</h1>
+          </Sider>
+          <Layout>
+            <Header>Header</Header>
+            <Content>
+              {' '}
+              <Button type="primary">Test override theme with craco</Button>
+            </Content>
+            <Footer>Footer</Footer>
+          </Layout>
+        </Layout>
+      </React.Fragment>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Main from './components/main/main';
+import Main from './components/Main-components/Main';
 
 ReactDOM.render(
   <React.StrictMode>
